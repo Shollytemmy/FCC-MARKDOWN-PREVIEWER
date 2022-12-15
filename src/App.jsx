55b1449ae42a4e32bfa70919aea90f53f026dd03
Bunyameen <bunyameen@gmail.com>
2022-12-15 03:23:18 +0100

    MarkDown: markdown installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
 import { useState } from 'react'
 import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
 import './App.css'
 
+const markdown = `# heading 1
+## heading 2
+##### heading 4
+
+**This is a bold text**
+_this is a italic text_
+
+[GOOGLE]: (https://www.google.com/)
+> this is a blocquote text
+
+
+\`\`\` javascript
+const dectet = (name) => \`You are ${name}\`
+\`\`\`
+
+- item1
+- item2
+- item3
+
+
+![Markdown Logo](https://markdown-here.com/img/icon256.png)
+
+
+  
+  
+  `
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputValue, setInputValue] = useState({text: markdown}) 
+
+  
 
   return (
     <div className="App">
-      <ReactMarkdown># _Hello_, *World*</ReactMarkdown>
       
     </div>
   )
